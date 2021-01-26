7c63ddeda8f6cda2f770cbc29e67a435985625f7
David Ruiz Rovelo <druiz@lcs.on.ca>
2021-01-26 12:28:12 -0500

    Poster design beginning with jail bars, 400 x 400

--- Playground.playground/Pages/Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Poster.xcplaygroundpage/Contents.swift
@@ -35,10 +35,52 @@ PlaygroundPage.current.liveView = canvas
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
 
  */
+canvas.highPerformance = true
+
 canvas.drawAxes(withScale: true, by: 50)
 
-canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
+//Background colour
+canvas.fillColor = Color(hue: 10, saturation: 10, brightness: 10, alpha: 100)
 
+// Draw my background
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: canvas.width, height: canvas.height)
+
+canvas.drawShapesWithBorders = true
+
+
+canvas.drawLine(from: Point(x:50 ,y: 100 ), to: Point(x:350 ,y:100 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:50 ,y: 200 ), to: Point(x:350 ,y:200 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:50 ,y: 300 ), to: Point(x:350 ,y:300 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:50 ,y: 100 ), to: Point(x:50 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:350 ,y: 100 ), to: Point(x:350 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:50 ,y:400 ), to: Point(x:350 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:100 ,y: 100 ), to: Point(x:100 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:150 ,y: 100 ), to: Point(x:150 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:200 ,y: 100 ), to: Point(x:200 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:250 ,y: 100 ), to: Point(x:250 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:300 ,y: 100 ), to: Point(x:300 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:100 ,y: 100 ), to: Point(x:100 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:399 ,y: 100 ), to: Point(x:399 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:1 ,y: 100 ), to: Point(x:1 ,y:400 ), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:1 ,y:400 ), to: Point(x:400 ,y: 400), lineWidth: 5)
+
+canvas.drawLine(from: Point(x:1 ,y:100 ), to: Point(x:400 ,y: 100), lineWidth: 5)
+
+canvas.highPerformance = false
 /*:
  ## Show the Live View
  Don't see any results?
